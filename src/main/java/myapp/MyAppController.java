218e16c28b20d31c65d677c433ef912d08c2bfd4
purin-it <stanahas@amber.plala.or.jp>
2020-11-23 23:08:54 +0900

    Application Java renamed.

--- src/main/java/myapp/MyAppController.java
+++ src/main/java/myapp/MyAppController.java
@@ -9,7 +9,7 @@ public class MyAppController {
     
     @GetMapping("/")
     public String index(Model model) {
-    	model.addAttribute("message", "Hello Azure Spring. Updated!! Updated Again!!");
+    	model.addAttribute("message", "Hello Azure Spring.");
         return "index";
     }
     
